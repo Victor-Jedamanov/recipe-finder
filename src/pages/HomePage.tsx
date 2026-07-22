d447e7a8cd133e8f48206899978d4a5cca3e9664
victor-jedamanov <vjedamanov@gmail.com>
2026-07-22 00:34:11 -0400

    Add react-router, add temp HomePage, removed unused folders/files

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <>
+      <title>Recipe Finder</title>
+
+      <div className="home-page" >
+        This is the HomePage
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
