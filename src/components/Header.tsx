5829989ddf3f92157cfc9130c280a9de736b1dc6
victor-jedamanov <vjedamanov@gmail.com>
2026-07-24 13:31:11 -0400

    Added basic header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+      <div>
+        This is a header
+      </div>
+    </>
+  );
+}
+
+export default Header;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,12 @@
+import Header from '../components/Header';
 import './HomePage.css';
 
 function HomePage() {
   return (
     <>
       <title>Recipe Finder</title>
+
+      <Header />
 
       <div className="home-page" >
         This is the HomePage
